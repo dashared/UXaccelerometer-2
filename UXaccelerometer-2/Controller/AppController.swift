aa6794d12befdb732f786a2f65a9a3e83e4b8821
Anya <anna.mikhaleva@keemoji.com>
2020-03-27 14:08:42 +0300

    Add old controller

--- UXaccelerometer-2/Controller/AppController.swift
+++ UXaccelerometer-2/Controller/AppController.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import MultipeerConnectivity
+import UIKit
+
+class AppController : UIViewController, MCSessionDelegate, MCBrowserViewControllerDelegate {
+
+    
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        
+    }
+
+}
